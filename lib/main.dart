94776017ba272bbe5ec69f16fec6bab7c34503a7
kero <kyrellous.basheer@gmail.com>
2023-04-15 18:29:11 +0200

    use Material3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:calala_market/screens/menu_screen/menu_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() =>
-    runApp(MaterialApp(home: const MenuScreen(), theme: ThemeData.dark()));
+void main() => runApp(MaterialApp(
+      home: const MenuScreen(),
+      theme: ThemeData.dark(
+        useMaterial3: true,
+      ),
+    ));

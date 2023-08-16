22fd14b7bd06342a1db3359001334356e81bac98
KERO <kyrellous.basheer@gmail.com>
2023-08-16 15:04:32 +0300

    update small icon btn widget

--- lib/screens/shared_widgets/small_icon_btn.dart
+++ lib/screens/shared_widgets/small_icon_btn.dart
@@ -1,44 +1,46 @@
-import 'dart:math';
-
-import 'package:calala_market/constants.dart';
 import 'package:flutter/material.dart';
 
 class SmallIconBtn extends StatelessWidget {
   const SmallIconBtn({
     super.key,
     this.onPressed,
-    this.iconSize,
-    this.radius = 17,
     this.splashRadius,
+    this.radius = 14,
+    this.iconSize = 35,
     this.paddingRadius = 0,
+    this.bgColorIntensity = .1,
     required this.icon,
     required this.iconColor,
   });
   final VoidCallback? onPressed;
   final IconData icon;
   final Color? iconColor;
-  final double? iconSize;
+  final double iconSize;
+  final double bgColorIntensity;
   final double? splashRadius;
+
   final double radius;
   final double paddingRadius;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(paddingRadius),
-      child: IconButton(
-        iconSize: kScreenSize!.width / 20, // iconSize ?? radius,
-        splashRadius: max(max(radius / 2, splashRadius ?? 0), 17) + 3,
-        constraints: BoxConstraints(
-          maxWidth: radius,
-          maxHeight: radius,
-          minWidth: radius,
-          minHeight: radius,
-        ),
-        padding: EdgeInsets.zero,
-        onPressed: onPressed,
-        icon: Icon(icon, color: iconColor),
-      ),
-    );
+    return SizedBox(
+        width: iconSize,
+        height: iconSize,
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.all(0),
+            shape: const CircleBorder(),
+            backgroundColor: iconColor?.withOpacity(bgColorIntensity),
+            shadowColor: Colors.transparent,
+            elevation: 9,
+          ),
+          child: Icon(
+            icon,
+            color: iconColor,
+            size: iconSize - iconSize * .3,
+          ),
+        ));
   }
 }
